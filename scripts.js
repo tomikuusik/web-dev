c9c98f00a1ddf47ff2a0515e953eb8c1f2805484
Taavi Raudkiv <taaviraudkivi2@gmail.com>
2021-10-24 22:51:29 +0300

    Fixed merge confilcts

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showContext(){
+    var x = document.getElementById('contex-menu')
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none'
+    }
+}
